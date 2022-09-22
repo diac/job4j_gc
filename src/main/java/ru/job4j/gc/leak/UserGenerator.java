6dc6a675f711a5129dbf9c28dd16f72903272cc7
diac <nikolai.gladkikh@gmail.com>
2022-09-22 21:15:10 +0300

    Избавиться от статических полей: 2. Найти утечку памяти. [#504882 #345780]

--- src/main/java/ru/job4j/gc/leak/UserGenerator.java
+++ src/main/java/ru/job4j/gc/leak/UserGenerator.java
@@ -12,10 +12,10 @@ public class UserGenerator implements Generate {
     public static final String SEPARATOR = " ";
     public static final int NEW_USERS = 1000;
 
-    public static List<String> names;
-    public static List<String> surnames;
-    public static List<String> patrons;
-    private static Set<User> users = new HashSet<>();
+    public List<String> names;
+    public List<String> surnames;
+    public List<String> patrons;
+    private Set<User> users = new HashSet<>();
     private Random random;
 
     public UserGenerator(Random random) {
@@ -64,7 +64,7 @@ public class UserGenerator implements Generate {
         return randomUser;
     }
 
-    public static List<User> getUsers() {
+    public List<User> getUsers() {
         return new ArrayList<>(users);
     }
 }
